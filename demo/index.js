31e887727385517b232ed58fafe6ffdaf736422d
dianwu <elgoogdian@gmail.com>
2015-10-11 14:22:12 +0800

    Update service_worker.js path

--- demo/index.js
+++ demo/index.js
@@ -218,7 +218,7 @@ window.addEventListener('load', function() {
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/push_notification/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       // .then(initialiseState);
       console.log('load - 1');
   } else {
