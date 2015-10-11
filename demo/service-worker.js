61eb9562edda28e76e3f3c1a639ba257da916da6
dianwu <elgoogdian@gmail.com>
2015-10-11 15:07:25 +0800

    Try to get NAS authLogin.cgi response

--- demo/service-worker.js
+++ demo/service-worker.js
@@ -15,6 +15,49 @@ self.addEventListener('push', function(event) {
       tag: tag
     })
   );
+  
+   event.waitUntil(  
+    fetch('http://192.168.1.100:8080/cgi-bin/authLogin.cgi?sid=8o8gd85e').then(function(response) {  
+      if (response.status !== 200) {  
+        // Either show a message to the user explaining the error  
+        // or enter a generic message and handle the   
+        // onnotificationclick event to direct the user to a web page  
+        console.log('Looks like there was a problem. Status Code: ' + response.status);  
+      throw new Error();  
+      }
+
+      // Examine the text in the response  
+      return response.json().then(function(data) {  
+        if (data.error || !data.notification) {  
+          console.error('The API returned an error.', data.error);  
+          throw new Error();  
+        }  
+        console.log('response....',data);
+        var title = data.notification.title;  
+        var message = data.notification.message;  
+        var icon = data.notification.icon;  
+        var notificationTag = data.notification.tag;
+
+        return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+      });  
+    }).catch(function(err) {  
+      console.error('Unable to retrieve data', err);
+
+      var title = 'An error occurred';
+      var message = 'We were unable to get the information for this push message';  
+      var icon = URL_TO_DEFAULT_ICON;  
+      var notificationTag = 'notification-error';  
+      return self.registration.showNotification(title, {  
+          body: message,  
+          icon: icon,  
+          tag: notificationTag  
+        });  
+    })  
+  );  
 });
 
 
